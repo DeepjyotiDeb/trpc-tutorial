7e9ce6ad963df30bbbe30c07b220d7103b1562fe
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2022-10-16 09:43:23 +0530

    correction typo

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,36 +1,40 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import { CreateUserInput } from '../schema/user.schema'
-import { trpc } from '../utils/trpc'
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { CreateUserInput } from '../schema/user.schema';
+import { trpc } from '../utils/trpc';
 
 function RegisterPage() {
-  const router = useRouter()
-    const {handleSubmit, register} = useForm<CreateUserInput>()
-    const {mutate, error} = trpc.useMutation(['usersregister'], {
-      onSuccess: (success) => {
-        router.push('/login')
-      },
-    })
+  const router = useRouter();
+  const { handleSubmit, register } = useForm<CreateUserInput>();
+  const { mutate, error } = trpc.useMutation(['users.register'], {
+    onSuccess: (success) => {
+      router.push('/login');
+    },
+  });
 
-    function onSubmit(values: CreateUserInput) {
-      mutate(values)
-    }
+  function onSubmit(values: CreateUserInput) {
+    mutate(values);
+  }
   return (
     <>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {error && error.message}
-      <h1>Register</h1>
-      <input type='email' placeholder='janedoe@mail.com' {...register('email')}/>
-      <br/>
-      <input type='text' placeholder='name' {...register('name')}/>
-      <br/>
-      <button type='submit'>Submit</button>
-    </form>
-    <Link href='/login'>Login</Link>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {error && error.message}
+        <h1>Register</h1>
+        <input
+          type='email'
+          placeholder='janedoe@mail.com'
+          {...register('email')}
+        />
+        <br />
+        <input type='text' placeholder='name' {...register('name')} />
+        <br />
+        <button type='submit'>Submit</button>
+      </form>
+      <Link href='/login'>Login</Link>
     </>
-  )
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
